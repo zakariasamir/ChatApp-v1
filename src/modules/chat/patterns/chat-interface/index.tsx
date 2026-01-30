8816b8295f8084e5b5feb31bed026af4e5101bde
zakariasamir <zakariasamir155@gmail.com>
2026-01-30 14:49:25 +0100

    Add ChatInterface component to chat patterns module, integrating ChatLayout and FloatingActionButton for enhanced user interaction and modularity.

---
+++ src/modules/chat/patterns/chat-interface/index.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import { ChatLayout, FloatingActionButton } from "@/modules/chat/components";
+
+const ChatInterface: React.FC = () => {
+  return (
+    <>
+      <ChatLayout />
+      <FloatingActionButton />
+    </>
+  );
+};
+
+export default ChatInterface;
